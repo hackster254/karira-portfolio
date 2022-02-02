639156eedadaa0a8138abc2ea295a488624090b4
hackster254 <hackster.origin@gmail.com>
2022-02-02 17:40:57 +0300

    added emailjs to work with @hookform/resolver

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 
 import emailJs from 'emailjs-com';
 
+import { useForm } from 'react-hook-form';
+
+import { computedTypesResolver } from '@hookform/resolvers/computed-types';
+import Schema, { number, string } from 'computed-types';
+
 const FormStyle = styled.form`
   width: 100%;
   .form-group {
@@ -47,6 +52,22 @@ export default function ContactForm() {
   const [message, setMessage] = useState('');
   const [subject, setSubject] = useState('');
 
+  const schema = Schema({
+    name: string.min(1).error('name field is required'),
+
+    email: string.min(1).error('email field is required'),
+    message: string.min(1).error('message field is required'),
+    subject: string.min(1).error('subject field is required'),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: computedTypesResolver(schema),
+  });
+
   const templateParams = {
     name,
     email,
@@ -55,7 +76,7 @@ export default function ContactForm() {
   };
 
   function sendEmail(e) {
-    e.preventDefault();
+    // e.preventDefault();
 
     if (name !== '' && email !== '' && message !== '' && subject !== '') {
       emailJs
@@ -82,20 +103,26 @@ export default function ContactForm() {
       setMessage('');
     }
   }
+
+  const onSubmit = (data) => console.log(data);
+
+  console.log(errors);
   return (
     <>
-      <FormStyle>
+      <FormStyle onSubmit={handleSubmit(sendEmail)}>
         <div className="form-group">
           <label htmlFor="name">
             Your Name
             <input
               type="text"
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...register('name', { required: true })}
               id="name"
               name="name"
               value={name}
               onChange={(e) => setName(e.target.value)}
-              required
             />
+            <p>{errors.name?.message}</p>
           </label>
         </div>
         <div className="form-group">
@@ -103,12 +130,14 @@ export default function ContactForm() {
             Your Email
             <input
               type="email"
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...register('email', { required: true })}
               id="email"
               name="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              required
             />
+            <p>{errors.email?.message}</p>
           </label>
         </div>
         <div className="form-group">
@@ -116,12 +145,14 @@ export default function ContactForm() {
             Subject
             <input
               type="text"
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...register('subject', { required: true })}
               id="subject"
               name="subject"
               value={subject}
               onChange={(e) => setSubject(e.target.value)}
-              required
             />
+            <p>{errors.subject?.message}</p>
           </label>
         </div>
         <div className="form-group">
@@ -129,17 +160,17 @@ export default function ContactForm() {
             Your message
             <textarea
               type="text"
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...register('message', { required: true })}
               id="message"
               name="message"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
-              required
             />
+            <p>{errors.message?.message}</p>
           </label>
         </div>
-        <button type="submit" onClick={sendEmail}>
-          Send
-        </button>
+        <button type="submit">Send</button>
       </FormStyle>
     </>
   );
